485ba9b5a8538b09877e6a47f445dbf6f2410f38
Varkalas <Varkalas@gmail.com>
2017-08-01 18:48:12 -0700

    * Separated the constants from the main file * Updated my To-Do list * Started work on the GUI

--- README.md
+++ README.md
@@ -1,14 +1,27 @@
 # Tower-of-Hero
-A tool to assist in data gathering for Tower of Hero.
+A tool to assist in data gathering for Tower of Hero. Current only supports 1080x1920 and with ads at the top of the Records tab.
 
 # To-Do List
-* Tkinter UI
-* Ability to import an image of user's Records
-* Ability to copy the data to clipboard
-* Ability to open in Notepad or Excel
+* Acquire the Cumulative statistics by OCRing that word and then gathering the data underneath
+* GUI
+    - Tkinter: tree view
+        - Put images of the items in the list (modify display of current pictures?)
+    - Add other people's Records: append a column with their name at the top
+        - Browse files, enter name edit box, and match % (default is 80% for 1080x1920)
+        - Add options for image manipulation?
+    - Put corfe83's tier in the table
+    - Put my importance level in the table
+    - Save functionality
+    - Export to Excel
+    - Copy to clipboard?
+    - Add Tower of Hero icon
+* Make into a standalone executable using pip installer
+* Adjust for different resolutions
+    - Need to modify max_val threshold based on resolution
+    - Need to come up with another way of checking text below located images
 
 # Implemented Features
-* OpenCV template matching for decent quality Records images (1080x1920)
-* Finds the location of an item to know the order the player got it in
-* Tesseract OCR to determine the level of each item
-* Writes the item name, level, and acquisition order to a file with tab spacing between 
+* Finds where each item is in Records (OpenCV)
+    - Currently only tested for 1080x1920, haven't tested lower resolutions
+* Grabs the level of each item via optimal character recognition (Tesseract)
+* Writes the item name, level, and acquisition order to a file with tab spacing in between 
--- tower_of_hero.py
+++ tower_of_hero.py
@@ -3,70 +3,28 @@ import math
 import numpy
 from PIL import Image
 import pytesseract
+import tkinter
+from tkinter import ttk
 
+from constants import *
 from images import *
 
 
-GAME_ITEMS = [("A King's Crown", IMG_A_KINGS_CROWN),
-              ("Aegis", IMG_AEGIS),
-              ("Awakening Armor", IMG_AWAKENING_ARMOR),
-              ("Awakening Armor +1", IMG_AWAKENING_ARMOR_PLUS_1),
-              ("Awakening Sword", IMG_AWAKENING_SWORD),
-              ("Awakening Sword +1", IMG_AWAKENING_SWORD_PLUS_1),
-              ("Black Essence", IMG_BLACK_ESSENCE),
-              ("Blue Elixir", IMG_BLUE_ELIXIR),
-              ("Caduceus", IMG_CADUCEUS),
-              ("Claymore", IMG_CLAYMORE),
-              ("Coat of Gold", IMG_COAT_OF_GOLD),
-              ("Dark Boots", IMG_DARK_BOOTS),
-              ("Dark Gate", IMG_DARK_GATE),
-              ("Dark Knight Armor", IMG_DARK_KNIGHT_ARMOR),
-              ("Demon Eye", IMG_DEMON_EYE),
-              ("Durandal", IMG_DURANDAL),
-              ("Earth Armor", IMG_EARTH_ARMOR),
-              ("Excalibur", IMG_EXCALIBUR),
-              ("Fire Sword", IMG_FIRE_SWORD),
-              ("Flamberge", IMG_FLAMBERGE),
-              ("Flame Pot", IMG_FLAME_POT),
-              ("Freyr's Sword", IMG_FREYRS_SWORD),
-              ("Full Helmet", IMG_FULL_HELMET),
-              ("Full Plate", IMG_FULL_PLATE),
-              ("Gae Bolg", IMG_GAE_BOLG),
-              ("Gate", IMG_GATE),
-              ("Gold Box", IMG_GOLD_BOX),
-              ("Gold Vessels", IMG_GOLD_VESSELS),
-              ("Golden Gloves", IMG_GOLDEN_GLOVES),
-              ("Golden Rod", IMG_GOLDEN_ROD),
-              ("Golden Pot", IMG_GOLDEN_POT),
-              ("Green Elixir", IMG_GREEN_ELIXIR),
-              ("Guild Hat", IMG_GUILD_HAT),
-              ("Gungnir", IMG_GUNGNIR),
-              ("Halberd", IMG_HALBERD),
-              ("Hydra's Poison Arrows", IMG_HYDRAS_POISON_ARROWS),
-              ("Ice Pot", IMG_ICE_POT),
-              ("Lævateinn", IMG_LAEVATEINN),
-              ("Lance", IMG_LANCE),
-              ("Magic Lamp", IMG_MAGIC_LAMP),
-              ("Mistilteinn", IMG_MISTILTEINN),
-              ("Mithril Armour", IMG_MITHRIL_ARMOUR),
-              ("Mithril Sword", IMG_MITHRIL_SWORD),
-              ("Mjolnir", IMG_MJOLNIR),
-              ("Philosopher's Stone", IMG_PHILOSOPHERS_STONE),
-              ("Rapier", IMG_RAPIER),
-              ("Red Elixir", IMG_RED_ELIXIR),
-              ("Solomon's Key", IMG_SOLOMONS_KEY),
-              ("Solomon's Staff", IMG_SOLOMONS_STAFF),
-              ("Summoning Letter", IMG_SUMMONING_LETTER),
-              ("Tomahawk", IMG_TOMAHAWK),
-              ("Training Book", IMG_TRAINING_BOOK),
-              ("Wing Boots", IMG_WING_BOOTS)]
-
-
 if __name__ == "__main__":
+    # Initialize the GUI
+    '''root = tkinter.Tk()
+    tree = ttk.Treeview(root)
+    tree["columns"] = ("Item", "Name", "Tier", "Importance", "Order")'''
+
+    # Append the images to column 1 (alphabetical)
+
+    # Append the item names to column 2
+
     # Create a new file
     my_file = open("C:\\tmp\\item_location.txt", "w")
     my_file.close()
 
+    # TODO: make this section part of GUI button (e.g. Gather Data)
     # Go through each item
     for item in GAME_ITEMS:
         # Match the template and if it's >= 80%, run with it
@@ -100,3 +58,7 @@ if __name__ == "__main__":
             item_level_number = item_level.translate({ord(char): None for char in " LV"})
             with open("C:\\tmp\\item_location.txt", "a") as my_file:
                 my_file.writelines("%s\t%s\t%d\n" % (item[0], item_level_number, item_acquisition_number))
+
+    # Run the GUI
+    '''tree.pack()
+    root.mainloop()'''
